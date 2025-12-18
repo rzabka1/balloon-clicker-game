e18f38b77fab06a1dfda762e34ccc7f2372a9041
rzabka1 <jturovska@gmail.com>
2025-12-18 23:14:20 +0100

    Added start screen and language settings

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -3,13 +3,17 @@ extends Node3D
 var score:int = 0
 var min_balloons:int = 10
 var max_balloons:int = 20
+var languages:Array = ["en", "de", "cs", "sk"]
+
 @export var score_label:Label
+
 @onready var balloon_scene:PackedScene = preload("res://Scenes/balon.tscn")
 @onready var balloon_parent: Node = $Balloonsy
+@onready var starting_screen:CanvasLayer = $StartingScreen
 
 func _ready() -> void:
 	set_up_balloons(random_count(), random_coords())
-	TranslationServer.set_locale("cs")
+	TranslationServer.set_locale("en")
 
 func random_count() -> int:
 	return randi_range(min_balloons,max_balloons)
@@ -49,8 +53,31 @@ func on_balloon_popped(balloon_points:int):
 
 func update_score(points:int):
 	score += points
-	score_label.text = str("WYNIK: ",score)
+	score_label.text = str(": ",score)
 
 func check_and_reload():
 	if balloon_parent.get_child_count() == 1:
 		set_up_balloons(random_count(), random_coords())
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		starting_screen.show()
+
+func _on_play_button_pressed() -> void:
+	starting_screen.hide()
+	
+func _on_language_button_pressed() -> void:
+	var lang_length = languages.size()
+	var current_lang_id = languages.find(TranslationServer.get_locale())
+	var next_lang_id
+	if current_lang_id == lang_length-1:
+		next_lang_id = 0
+	else:
+		next_lang_id = current_lang_id + 1
+	change_language(languages[next_lang_id])
+
+func change_language(lang_code:String):
+	TranslationServer.set_locale(lang_code)
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
